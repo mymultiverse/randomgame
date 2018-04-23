a98e9f037604801e435a521f2061febdd3179f1b
Nebula <yrajsm@gmail.com>
2018-04-23 12:41:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,12 +26,10 @@ function startGame() {
 }
 
 function turnClick(square) {
-	if (typeof origBoard[square.target.id] == 'number'){
+	if (typeof origBoard[square.target.id] == 'number') {
 		turn(square.target.id, huPlayer)
-		if (!checkTie()) turn(bestSpot(), aiPlayer);
-
+		if (!checkWin(origBoard, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
 	}
-
 }
 
 function turn(squareId, player) {
@@ -42,72 +40,69 @@ function turn(squareId, player) {
 }
 
 function checkWin(board, player) {
-	let plays = board.reduce((a, e, i)=>
-		(e === player) ? a.concat(i):a,[]);
+	let plays = board.reduce((a, e, i) =>
+		(e === player) ? a.concat(i) : a, []);
 	let gameWon = null;
-	for (let [index, win] of winCombos.entries()){
-		if (win.every(elem => plays.indexOf(elem)>-1)){
-			gameWon = {index: index, player:player};
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
 			break;
 		}
 	}
 	return gameWon;
-
 }
 
-function gameOver(gameWon){
+function gameOver(gameWon) {
 	for (let index of winCombos[gameWon.index]) {
-		document.getElementById(index).style.backgroundColor=gameWon.player == huPlayer ? "blue":"red";
-		
+		document.getElementById(index).style.backgroundColor =
+			gameWon.player == huPlayer ? "blue" : "red";
 	}
 	for (var i = 0; i < cells.length; i++) {
-		cells[i].removeEventListener('click', turnClick, false)
+		cells[i].removeEventListener('click', turnClick, false);
 	}
-	declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.")
-
+	declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
 }
 
-function declareWinner(who){
+function declareWinner(who) {
 	document.querySelector(".endgame").style.display = "block";
-	document.querySelector(".endgame").innerText = who;
-
+	document.querySelector(".endgame .text").innerText = who;
 }
 
-function emptySquares(){
+function emptySquares() {
 	return origBoard.filter(s => typeof s == 'number');
 }
 
-function bestSpot(){
+function bestSpot() {
 	return minimax(origBoard, aiPlayer).index;
 }
 
-function checkTie(){
-	if (emptySquares().length == 0){
+function checkTie() {
+	if (emptySquares().length == 0) {
 		for (var i = 0; i < cells.length; i++) {
 			cells[i].style.backgroundColor = "green";
 			cells[i].removeEventListener('click', turnClick, false);
 		}
-		declareWinner("Tie Game")
+		declareWinner("Tie Game!")
 		return true;
 	}
 	return false;
 }
 
 function minimax(newBoard, player) {
-	var availSpots = emptySquares(newBoard);
+	var availSpots = emptySquares();
 
-	if (checkWin(newBoard, player)){
-		return {score : -10};
-	} else if (checkWin(newBoard, aiPlayer)){
-		return {score: 20};
-	} else if (availSpots.length === 0){
+	if (checkWin(newBoard, huPlayer)) {
+		return {score: -10};
+	} else if (checkWin(newBoard, aiPlayer)) {
+		return {score: 10};
+	} else if (availSpots.length === 0) {
 		return {score: 0};
 	}
 	var moves = [];
 	for (var i = 0; i < availSpots.length; i++) {
 		var move = {};
 		move.index = newBoard[availSpots[i]];
-		newBoard[availSpots[i]]= player;
+		newBoard[availSpots[i]] = player;
 
 		if (player == aiPlayer) {
 			var result = minimax(newBoard, huPlayer);
@@ -116,21 +111,24 @@ function minimax(newBoard, player) {
 			var result = minimax(newBoard, aiPlayer);
 			move.score = result.score;
 		}
+
 		newBoard[availSpots[i]] = move.index;
+
 		moves.push(move);
 	}
+
 	var bestMove;
-	if (player === aiPlayer){
+	if(player === aiPlayer) {
 		var bestScore = -10000;
-		for (var i = 0; i < moves.length; i++) {
+		for(var i = 0; i < moves.length; i++) {
 			if (moves[i].score > bestScore) {
 				bestScore = moves[i].score;
 				bestMove = i;
 			}
 		}
-	}else {
+	} else {
 		var bestScore = 10000;
-		for (var i = 0; i < moves.length; i++) {
+		for(var i = 0; i < moves.length; i++) {
 			if (moves[i].score < bestScore) {
 				bestScore = moves[i].score;
 				bestMove = i;
